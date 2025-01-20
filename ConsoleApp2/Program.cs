57762b00e28ae98c9466c231299bb2a29d39fec3
JamesLimos <limosjamesfrancis@gmail.com>
2025-01-20 13:31:42 -0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ Author: James Limos
+ Course: COMP-003A
+ Faculty: Jonathan Cruz
+ Purpose: A program demonstrating statements, variables, and operators.
+*/
+// Namespace level
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        //main level
+        static void Main(string[] args)
+        {
+            string userName;
+            int useAge;
+            double productprice;
+            bool isStudent;
+
+            Console.Write("Enter your name: ");
+            userName = Console.ReadLine();
+
+            Console.Write("enter your age: ");
+            useAge = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the price of the product: ");
+            productprice = double.Parse(Console.ReadLine());
+
+            Console.Write("Are you a student? (true/false)");
+            isStudent = bool.Parse(Console.ReadLine());
+
+            int futureAge = useAge + 5;
+            bool isAdult = useAge >= 18;
+            bool isAdultStudent = isStudent && isAdult;
+
+            Console.WriteLine($"\nHello, {userName}!");
+            Console.WriteLine($"You are currently {useAge} years old. In 5 years, you will be {futureAge}.");
+            Console.WriteLine($"Are you 18 or older? {isAdult}");
+            Console.WriteLine($"Are you both a Student and 18 or older? {isAdultStudent}");
+            
+        }
+    }
+}
